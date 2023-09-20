9c9fd6d2ef7756a355432e50af6011b495b0e984
nyaruku <revilogd3@gmail.com>
2023-09-20 12:14:50 +0300

    Initial commit

---
+++ osu_cpp.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <curl/curl.h>
+#include <json.hpp>
+#include <cpr/cpr.h>
+
+class osu_cpp {
+
+public:
+
+	class osuApiV2 {
+
+	public:
+		
+		const char* authToken;
+		const char* client_id;
+	};
+};
