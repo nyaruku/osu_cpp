59e4eb208a3b9ce4e3b1b83247c256ae17ae7fc2
nyaruku <revilogd3@gmail.com>
2023-09-20 12:57:33 +0300

    Updated Header

--- osu_cpp.h
+++ osu_cpp.h
@@ -13,7 +13,9 @@ public:
 		std::string client_id;
 		std::string client_secret;
 
-		void Auth() {
+		// Can be also used to re-authenticate
+		// Returns response status. Ex: "HTTP/1.1 200 OK"
+		std::string Auth() {
 			nlohmann::json AuthResult;
 			std::string body = R"({"client_id":)" + client_id + R"(, "client_secret":")" + client_secret + R"(", "grant_type":"client_credentials", "scope":"public"})";
 			auto r = cpr::Post(cpr::Url{ "https://osu.ppy.sh/oauth/token" },
@@ -21,8 +23,12 @@ public:
 				body
 				},
 				cpr::Header{ { "Content-Type", "application/json" } });
+			if (r.status_code == 0 || r.status_code >= 400)
+				return r.status_line;
+
 			AuthResult = nlohmann::json::parse(r.text);
 			authToken = "Bearer " + AuthResult.value("access_token", "");
+			return r.status_line;
 		}
 	};
 };
