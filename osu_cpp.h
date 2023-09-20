aeae2c36d279db630e3bfe80e15d1c2c9d1994f5
nyaruku <revilogd3@gmail.com>
2023-09-20 13:03:25 +0300

    Update osu_cpp.h

--- osu_cpp.h
+++ osu_cpp.h
@@ -15,6 +15,7 @@ public:
 
 		// Can be also used to re-authenticate
 		// Returns response status. Ex: "HTTP/1.1 200 OK"
+		// ^^^ You don't need to get the returned value if don't want
 		std::string Auth() {
 			nlohmann::json AuthResult;
 			std::string body = R"({"client_id":)" + client_id + R"(, "client_secret":")" + client_secret + R"(", "grant_type":"client_credentials", "scope":"public"})";
@@ -23,7 +24,10 @@ public:
 				body
 				},
 				cpr::Header{ { "Content-Type", "application/json" } });
-			if (r.status_code == 0 || r.status_code >= 400)
+			if (r.status_code == 0)
+				return r.error.message;
+
+			if (r.status_code >= 400)
 				return r.status_line;
 
 			AuthResult = nlohmann::json::parse(r.text);
