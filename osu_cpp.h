e65581b0e07696e61f29068bcd2c2870a758c5fb
nyaruku <revilogd3@gmail.com>
2023-09-20 12:35:18 +0300

    Update osu_cpp.h

--- osu_cpp.h
+++ osu_cpp.h
@@ -4,14 +4,25 @@
 #include <cpr/cpr.h>
 
 class osu_cpp {
-
 public:
 
 	class osuApiV2 {
-
 	public:
-		
-		const char* authToken;
-		const char* client_id;
+
+		std::string authToken;
+		std::string client_id;
+		std::string client_secret;
+
+		void Auth() {
+			nlohmann::json AuthResult;
+			std::string body = R"({"client_id":)" + client_id + R"(, "client_secret":")" + client_secret + R"(", "grant_type":"client_credentials", "scope":"public"})";
+			auto r = cpr::Post(cpr::Url{ "https://osu.ppy.sh/oauth/token" },
+				cpr::Body{
+				body
+				},
+				cpr::Header{ { "Content-Type", "application/json" } });
+			AuthResult = nlohmann::json::parse(r.text);
+			authToken = "Bearer " + AuthResult.value("access_token", "");
+		}
 	};
 };
